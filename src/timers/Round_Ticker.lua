5e68dc66be458560477b5c5e9b1feaabfd7d9f15
Michael Edwards <mledwards@carilionclinic.org>
2024-08-19 13:33:00 -0400

    trying adding function name to if statement for hp ticker

--- src/timers/Round_Ticker.lua
+++ src/timers/Round_Ticker.lua
@@ -1,15 +1,15 @@
   local vitals = ThreeKlient.mip.vitals
-  if vitals.hpcur and vitals.hpmax then
-    -- checkHP(vitals.hpcur, vitals.hpmax)
+  if vitals.hpcur and vitals.hpmax and checkHP then
+    checkHP(vitals.hpcur, vitals.hpmax)
   end
-  if vitals.spcur and vitals.spmax then
-    -- checkSP(vitals.spcur, vitals.spmax)
+  if vitals.spcur and vitals.spmax and checkSP then
+    checkSP(vitals.spcur, vitals.spmax)
   end
-  if vitals.gp1cur and vitals.gp1max then
-    -- checkFort(vitals.gp1cur, vitals.gp1max)
+  if vitals.gp1cur and vitals.gp1max and checkFort then
+    checkFort(vitals.gp1cur, vitals.gp1max)
   end
-  if vitals.gp2cur and vitals.gp2max then
-    -- checkControlLevel(tonumber(vitals.gp2cur), tonumber(vitals.gp2max))
+  if vitals.gp2cur and vitals.gp2max and checkControlLevel then
+    checkControlLevel(tonumber(vitals.gp2cur), tonumber(vitals.gp2max))
   end
   if vitals.enemyname and (string.len(vitals.enemyname) == 1 or vitals.enemyname == "Enemy") then
     inCombat = false
